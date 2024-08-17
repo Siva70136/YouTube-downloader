37bbda720a5fdddc0b1a1c1538e74bbe081ca0c1
Siva70136 <cheerlasivamallikharjuna@.com>
2024-08-17 23:34:52 +0530

    modified backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -1,5 +1,6 @@
 import yt_dlp
 import os
+import uvicorn
 from fastapi import FastAPI,Form
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -14,6 +15,8 @@ app.add_middleware(
     allow_methods=["*"],  
     allow_headers=["*"],  
 )
+
+        
 
 class URLRequest(BaseModel):
     url: str
@@ -79,8 +82,13 @@ def getDetails(request: URLRequest):
         "info":result,
     }
     
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    print(f"Starting server on port {port}...")
+
+#if __name__ == "__main__":
+port = int(os.environ.get("PORT", 8000))
+print(f"PORT environment variable: {port}")
+print(f"Starting server on port {port}...")
+try:
     uvicorn.run(app, host="0.0.0.0", port=port)
+    print("Server started successfully.")
+except Exception as e:
+    print(f"Failed to start server: {e}")
